f48699ec2272aef5d1b9f49053bf115f481bce45
Cha_Y_S <vaite714@gmail.com>
2021-05-19 03:41:31 +0900

    추가: 소수만들기 스켈레톤 추가

---
+++ 소수 만들기/main.cpp
@@ -0,0 +1,15 @@
+#include "solution.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  vector<int> nums;
+  
+  cout << solution(nums = {1,2,3,4}) << endl;
+
+  cout << solution(nums = {1,2,7,6,4}) << endl;
+
+  return 0;
+}
