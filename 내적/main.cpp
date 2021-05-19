3a565df235df8f09ff0b0986ba839d59e5888667
Cha_Y_S <vaite714@gmail.com>
2021-05-20 01:10:52 +0900

    추가: 내적 스켈레톤 추가

---
+++ 내적/main.cpp
@@ -0,0 +1,12 @@
+#include "solution.h"
+
+int main(){
+  vector<int> a;
+  vector<int> b;
+
+  cout << solution(a={1,2,3,4}, b={-3,-1,0,2}) << endl;
+
+  cout << solution(a={-1,0,1}, b={1,0,-1}) << endl;
+
+  return 0;
+}
