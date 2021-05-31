82f41e56c494ba7d2636ba07b12ebfb22d3f3d94
Cha_Y_S <vaite714@gmail.com>
2021-06-01 03:09:51 +0900

    추가: 정수 내림차순으로 배치하기 스켈레톤 추가

---
+++ Programmers-Level1/정수 내림차순으로 배치하기/main.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+
+int main(){
+  long long n;
+  
+  cout << solution(n=118372) << endl;
+
+  return 0;
+}
---
+++ Programmers-Level1/정수 내림차순으로 배치하기/solution.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+long long solution(long long n);
