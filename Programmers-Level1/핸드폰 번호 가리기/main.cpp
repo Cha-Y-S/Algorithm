b33a9e47cf01d278cd8db21fc79085e80edb46c9
Cha_Y_S <vaite714@gmail.com>
2021-06-04 02:59:13 +0900

    추가: 핸드폰 번호 가리기 스켈레톤 추가

---
+++ Programmers-Level1/핸드폰 번호 가리기/main.cpp
@@ -0,0 +1,10 @@
+#include "solution.h"
+
+int main(){
+
+  cout << "\"" << solution("01033334444") << "\"" << endl;
+
+  cout << "\"" << solution("027778888") << "\"" << endl;
+
+  return 0;
+}
---
+++ Programmers-Level1/핸드폰 번호 가리기/solution.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string phone_number);
