b11bb4fe9ef92e2f1d11bdda719749da022521fc
Cha_Y_S <vaite714@gmail.com>
2021-06-04 02:53:09 +0900

    추가: 2021/06/04 프로그래머스 Level 1 문제 추가

---
+++ Programmers-Level1/행렬의 덧셈/solution.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+  vector<vector<int>> answer;
+
+  int rowSize = arr1.size();
+  int colSize = arr1[0].size();
+
+  for(int i = 0; i < rowSize; i++){
+    vector<int> temp;
+    for(int j = 0; j < colSize; j++){
+      temp.push_back(arr1[i][j]+ arr2[i][j]);
+    }
+    answer.push_back(temp);
+  }
+
+  return answer;
+}
