ffa969b35fb6bcb7ef197a03171b75c86fdd720a
Cha_Y_S <vaite714@gmail.com>
2021-06-08 19:02:05 +0900

    추가: 폰켓몬 스켈레톤 추가

---
+++ Programmers-Level1/폰켓몬/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  vector<int> nums;
+
+  cout << solution(nums = {3, 1, 2, 3}) << endl;
+
+  cout << solution(nums = {3, 3, 3, 2, 2, 4}) << endl;
+
+  cout << solution(nums = {3, 3, 3, 2, 2, 2}) << endl;
+
+  return 0;
+}
