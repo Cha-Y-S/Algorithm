113a1cf1f454039a9edddd9af650b3a5e6331a22
Cha_Y_S <vaite714@gmail.com>
2021-06-14 19:47:44 +0900

    추가: 신규 아이디 추천 스켈레톤 추가

---
+++ Programmers-Level1/신규 아이디 추천/main.cpp
@@ -0,0 +1,18 @@
+#include "solution.h"
+
+int main() {
+  string new_id;
+  string temp = "12345";
+
+  cout << solution(new_id = "...!@BaT#*..y.abcdefghijklm") << endl;
+
+  cout << solution(new_id = "z-+.^.") << endl;
+
+  cout << solution(new_id = "=.=") << endl;
+
+  cout << solution(new_id = "123_.def") << endl;
+
+  cout << solution(new_id = "abcdefghijklmn.p") << endl;
+
+  return 0;
+}
