586783b3d24dc51de4f00e2c9a920338dd6a7bfc
Cha_Y_S <vaite714@gmail.com>
2021-06-16 17:18:55 +0900

    추가: 크레인 인형뽑기 게임 스켈레톤 추가

---
+++ Programmers-Level1/크레인 인형뽑기 게임/main.cpp
@@ -0,0 +1,10 @@
+#include "solution.h"
+
+int main() {
+  vector<vector<int>> board;
+  vector<int> moves;
+
+  cout << solution(board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}}, moves = {1, 5, 3, 5, 1, 2, 1, 4}) << endl;
+
+  return 0;
+}
