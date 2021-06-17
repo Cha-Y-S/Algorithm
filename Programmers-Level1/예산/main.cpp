ac5960d574734b994ae265e098cc0418ca59d09f
Cha_Y_S <vaite714@gmail.com>
2021-06-17 16:23:17 +0900

    추가: 예산 스켈레톤 추가

---
+++ Programmers-Level1/예산/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  vector<int> d;
+
+  cout << solution(d = {1, 3, 2, 5, 4}, 9) << endl;
+
+  cout << solution(d = {2, 2, 3, 3}, 10) << endl;
+
+  return 0;
+}
