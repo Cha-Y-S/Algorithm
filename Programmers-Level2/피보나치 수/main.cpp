fb970f0e814d65bc9758621a7ee83b4a0eae55bb
Cha_Y_S <vaite714@gmail.com>
2021-06-22 16:48:40 +0900

    추가: 피보나치 수 스켈레톤 추가

---
+++ Programmers-Level2/피보나치 수/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  cout << solution(3) << endl;
+
+  cout << solution(5) << endl;
+
+  cout << solution(44) << endl;
+
+  cout << solution(45) << endl;
+
+  return 0;
+}
