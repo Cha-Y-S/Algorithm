ff653e8f016e0ce31774acc58b3ad975e78f631a
Cha_Y_S <vaite714@gmail.com>
2021-06-23 16:51:25 +0900

    추가: 최솟값 만들기 스켈레톤 추가

---
+++ Programmers-Level2/최솟값 만들기/main.cpp
@@ -0,0 +1,12 @@
+#include "solution.h"
+
+int main() {
+  vector<int> A;
+  vector<int> B;
+
+  cout << solution(A = {1, 4, 2}, B = {5, 4, 4}) << endl;
+
+  cout << solution(A = {1, 2}, B = {3, 4}) << endl;
+
+  return 0;
+}
---
+++ Programmers-Level2/최솟값 만들기/solution.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B);
