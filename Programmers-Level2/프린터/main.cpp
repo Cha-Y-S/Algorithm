3d059656732312cc51513c34b43d374adb31edc0
Cha_Y_S <vaite714@gmail.com>
2021-06-24 19:07:48 +0900

    추가: 프린터 스켈레톤 추가

---
+++ Programmers-Level2/프린터/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  vector<int> priorities;
+
+  cout << solution(priorities = {2, 1, 3, 2}, 2) << endl;
+
+  cout << solution(priorities = {1, 1, 9, 1, 1, 1}, 0) << endl;
+
+  return 0;
+}
