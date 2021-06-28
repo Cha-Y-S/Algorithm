a62f391052bb87225f82f710fa8d1fcbde2ed8ce
Cha_Y_S <vaite714@gmail.com>
2021-06-28 18:25:44 +0900

    추가: 더 맵게 스켈레톤 추가

---
+++ Programmers-Level2/더 맵게/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  vector<int> scoville;
+
+  cout << solution(scoville = {1, 2, 3, 9, 10, 12}, 7) << endl;
+
+  cout << solution(scoville = {1, 2}, 7) << endl;
+
+  cout << solution(scoville = {8, 9, 10, 11, 12, 13}, 7) << endl;
+
+  return 0;
+}
