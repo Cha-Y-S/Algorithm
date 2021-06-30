d193da3f7acfe2e886d1c0f9d875deed490eee49
Cha_Y_S <vaite714@gmail.com>
2021-06-30 19:10:20 +0900

    추가: 최댓값과 최솟값 스켈레톤 추가

---
+++ Programmers-Level2/최댓값과 최솟값/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("1 2 3 4") << endl;
+
+  cout << solution("-1 -2 -3 -4") << endl;
+
+  cout << solution("-1 -1") << endl;
+
+  return 0;
+}
