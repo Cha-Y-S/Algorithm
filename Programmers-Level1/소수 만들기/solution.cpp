ccbe015c3025ef1181fb916d2f04a7cf4dc90a75
firemanCha <vaite714@gmail.com>
2021-07-04 22:24:07 +0900

    수정: 소수 만들기 오타 수정

--- Programmers-Level1/소수 만들기/solution.cpp
+++ Programmers-Level1/소수 만들기/solution.cpp
@@ -1,7 +1,7 @@
-#include <vector>
-#include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,34 +10,35 @@ bool isPrime(int n);
 int solution(vector<int> nums) {
   int answer = 0;
   int len = nums.size();
-  vector<bool>v(len-3, false);;
-
+  vector<bool> v(len - 3, false);
 
   v.insert(v.end(), 3, true);
 
   do {
-    vector<int>partial;
-    for(int i = 0; i < nums.size(); i++){
-      if(v[i]) { partial.push_back(nums[i]); }
+    vector<int> partial;
+    for (int i = 0; i < nums.size(); i++) {
+      if (v[i]) {
+        partial.push_back(nums[i]);
+      }
     }
     int sum = 0;
 
-    for(int i = 0; i < partial.size(); i++){
+    for (int i = 0; i < partial.size(); i++) {
       sum += partial[i];
     }
 
-    if(isPrime(sum)) answer++;
+    if (isPrime(sum)) answer++;
 
-  } while(next_permutation(v.begin(), v.end()));
+  } while (next_permutation(v.begin(), v.end()));
 
   return answer;
 }
 
-bool isPrime(int n){
+bool isPrime(int n) {
   if (n <= 1) return false;
   if (n % 2 == 0) return false;
-  for(int i = 3; i <= sqrt(n); i += 2){
-    if(n % i == 0) return false;
+  for (int i = 3; i <= sqrt(n); i += 2) {
+    if (n % i == 0) return false;
   }
   return true;
 }
