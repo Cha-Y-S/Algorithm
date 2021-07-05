b80e190553cc03b9623951818e3a110312ce1f4a
firemanCha <vaite714@gmail.com>
2021-07-05 21:27:00 +0900

    추가: 큰 수 만들기 스켈레톤 추가

---
+++ Programmers-Level2/큰 수 만들기/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("1924", 2) << endl;
+
+  cout << solution("1231234", 3) << endl;
+
+  cout << solution("4177252841", 4) << endl;
+
+  return 0;
+}
