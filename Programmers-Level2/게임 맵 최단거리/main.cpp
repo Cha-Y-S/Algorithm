6690fab0362c48500c7780b4fc3e6cd1a5e62fcc
firemanCha <vaite714@gmail.com>
2021-07-08 19:23:54 +0900

    추가: 게임 맵 최단거리 스켈레톤 추가

---
+++ Programmers-Level2/게임 맵 최단거리/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  vector<vector<int>> maps;
+
+  cout << solution(maps = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}}) << endl;
+
+  cout << solution(maps = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}}) << endl;
+
+  return 0;
+}
---
+++ Programmers-Level2/게임 맵 최단거리/solution.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> maps);
