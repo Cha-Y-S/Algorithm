5ae562ff2031c18511d7703d505321f7955b994e
firemanCha <vaite714@gmail.com>
2021-07-11 18:17:50 +0900

    추가: 숫자 문자열과 영단어 스켈레톤 추가

---
+++ Programmers-Level1/숫자 문자열과 영단어/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("one4seveneight") << endl;
+
+  cout << solution("23four5six7") << endl;
+
+  cout << solution("2three45sixseven") << endl;
+
+  cout << solution("123") << endl;
+
+  return 0;
+}
