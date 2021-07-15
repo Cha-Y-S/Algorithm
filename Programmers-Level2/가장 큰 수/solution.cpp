1bcc1cd897eecb63c49787469a68935cf7e1cee8
firemanCha <vaite714@gmail.com>
2021-07-15 20:03:17 +0900

    추가: 2021/07/15 프로그래머스 Level 2 문제 추가

---
+++ Programmers-Level2/가장 큰 수/solution.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int comp(string a, string b) {
+  return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+  string answer = "";
+  vector<string> sNums;
+
+  for (int i = 0; i < numbers.size(); i++) {
+    sNums.push_back(to_string(numbers[i]));
+  }
+
+  sort(sNums.begin(), sNums.end(), greater<string>());
+
+  for (int i = 0; i < sNums.size(); i++)
+    answer += sNums[i];
+
+  answer = answer[0] == '0' ? "0" : answer;
+
+  return answer;
+}
