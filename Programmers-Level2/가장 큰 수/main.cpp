a5161e72594e950eda38dce436150624a7196e00
firemanCha <vaite714@gmail.com>
2021-07-15 20:02:58 +0900

    추가: 가장 큰 수 스켈레톤 추가

---
+++ Programmers-Level2/가장 큰 수/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  vector<int> numbers;
+
+  cout << "\"" << solution(numbers = {6, 10, 2}) << "\"" << endl;
+
+  cout << "\"" << solution(numbers = {3, 30, 34, 5, 9}) << "\"" << endl;
+
+  return 0;
+}
