af3f9da244156ee3c97be3bb3a46891238d6a1b0
Cha_Y_S <vaite714@gmail.com>
2021-07-16 15:55:16 +0900

    추가: 문자열 압축 스켈레톤 추가

---
+++ Programmers-Level2/문자열 압축/main.cpp
@@ -0,0 +1,15 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("aabbaccc") << endl;
+
+  cout << solution("ababcdcdababcdcd") << endl;
+
+  cout << solution("abcabcdede") << endl;
+
+  cout << solution("abcabcabcabcdededededede") << endl;
+
+  cout << solution("xababcdcdababcdcd") << endl;
+
+  return 0;
+}
