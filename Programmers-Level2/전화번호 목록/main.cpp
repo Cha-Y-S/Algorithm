66a0e543b7fa712c3235d5264c678f8f4dd31d92
Cha_Y_S <vaite714@gmail.com>
2021-07-19 04:17:43 +0900

    추가: 전화번호 목록 스켈레톤 추가

---
+++ Programmers-Level2/전화번호 목록/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  vector<string> phone_book;
+
+  cout << solution(phone_book = {"119", "97674223", "1195524421"}) << endl;
+
+  cout << solution(phone_book = {"123", "456", "789"}) << endl;
+
+  cout << solution(phone_book = {"12", "123", "1235", "567", "88"}) << endl;
+
+  return 0;
+}
