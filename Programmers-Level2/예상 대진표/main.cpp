43ab2cf31c93bc3004c2901e205e41f610377c8d
Cha_Y_S <vaite714@gmail.com>
2021-07-21 21:10:30 +0900

    추가: 예상 대진표 스켈레톤 추가

---
+++ Programmers-Level2/예상 대진표/main.cpp
@@ -0,0 +1,7 @@
+#include "solution.h"
+
+int main() {
+  cout << solution(8, 4, 7) << endl;
+
+  return 0;
+}
---
+++ Programmers-Level2/예상 대진표/solution.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b);
