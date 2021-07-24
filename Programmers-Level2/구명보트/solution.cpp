2b269e63b087ad0b4871fac7d79a8a85b3ac02ad
firemanCha <vaite714@gmail.com>
2021-07-24 18:25:01 +0900

    추가: 구명보트 solution 추가

---
+++ Programmers-Level2/구명보트/solution.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+  int answer = 0;
+  int curWeight = 0;
+
+  sort(people.begin(), people.end(), greater<int>());
+
+  return answer;
+}
