e5638f5c5af83cd6276730bb3b8a7bec287da391
firemanCha <vaite714@gmail.com>
2021-07-24 17:49:49 +0900

    추가: 구명보트 스켈레톤 추가

---
+++ Programmers-Level2/구명보트/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  vector<int> people;
+
+  cout << solution(people = {70, 50, 80, 50}, 100) << endl;
+
+  cout << solution(people = {70, 80, 50}, 100) << endl;
+
+  return 0;
+}
