4ddbe1537859487ccfaded3ad137ca0fd45cbe1a
Cha_Y_S <vaite714@gmail.com>
2021-07-25 15:04:59 +0900

    추가: 2021/07/25 프로그래머스 Level 2 문제 추가

---
+++ Programmers-Level2/숫자의 표현/solution.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+  int answer = 0;
+  int sum = 0;
+
+  for (int i = 1; i <= n / 2; i++) {
+    sum += i;
+    for (int j = i + 1; j <= n / 2 + 1; j++) {
+      sum += j;
+      if (sum > n) {
+        sum = 0;
+        break;
+      } else if (sum == n) {
+        sum = 0;
+        answer++;
+        break;
+      }
+    }
+  }
+
+  return ++answer;
+}
