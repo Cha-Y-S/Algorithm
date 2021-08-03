26b72d8460389c809869d2e2ebbeaf55d50bd965
Cha_Y_S <vaite714@gmail.com>
2021-08-03 21:16:41 +0900

    추가: 뉴스 클러스터링 스켈레톤 추가

---
+++ Programmers-Level2/뉴스 클러스터링/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("FRANCE", "french") << endl;
+
+  cout << solution("handshake", "shake hands") << endl;
+
+  cout << solution("aa1+aa2", "AAAA12") << endl;
+
+  cout << solution("E=M*C^2", "e=m*c^2") << endl;
+
+  return 0;
+}
