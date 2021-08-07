2d5dca628e692af216ec555f320494eec86f661a
Cha_Y_S <vaite714@gmail.com>
2021-08-08 03:38:25 +0900

    추가: 2021/08/08 프로그래머스 Level 2 문제 추가

--- Programmers-Level2/2개 이하로 다른 비트/solution.cpp
+++ Programmers-Level2/2개 이하로 다른 비트/solution.cpp
@@ -1,5 +1,3 @@
-#include <bitset>
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -9,19 +7,20 @@ vector<long long> solution(vector<long long> numbers) {
   vector<long long> answer;
 
   for (int i = 0; i < numbers.size(); i++) {
-    for (int j = numbers[i] + 1;; j++) {
-      bitset<50> bit(numbers[i] ^ j);
+    int digit = 0;
+    long long number = numbers[i];
 
-      int cnt = 0;
-      for (int k = 0; k < bit.size(); k++) {
-        if (bit[k]) cnt++;
-      }
-
-      if (cnt > 0 && cnt <= 2) {
-        answer.push_back(j);
-        break;
-      }
+    while (number & (1LL << digit)) {
+      digit++;
     }
+
+    if (digit) {
+      long long temp = 1LL << (digit - 1);
+      number = number + (1LL << digit) - temp;
+    } else {
+      number += 1;
+    }
+    answer.push_back(number);
   }
 
   return answer;
