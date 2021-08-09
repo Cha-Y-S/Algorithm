2a5b2a85ec9d542be22e9696c1e73f99c162b05c
Cha_Y_S <vaite714@gmail.com>
2021-08-10 03:43:43 +0900

    추가: 스킬트리 스켈레톤 추가

---
+++ Programmers-Level2/스킬트리/main.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+
+int main() {
+  vector<string> skill_trees;
+
+  cout << solution("CBD", skill_trees = {"BACDE", "CBADF", "AECB", "BDA"}) << endl;
+
+  return 0;
+}
---
+++ Programmers-Level2/스킬트리/solution.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees);
