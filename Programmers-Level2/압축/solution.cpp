f3a122f99abad0bc5be9a9e86dfe91d37e4c9bd7
Cha_Y_S <vaite714@gmail.com>
2021-08-09 16:43:16 +0900

    추가: 2021/08/09 프로그래머스 Level 2 문제 추가

--- Programmers-Level2/압축/solution.cpp
+++ Programmers-Level2/압축/solution.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+bool isInVoca(vector<string> voca, string s);
+int getVocaIndex(vector<string> voca, string s);
+
 vector<int> solution(string msg) {
   vector<int> answer;
   vector<string> voca;
@@ -14,6 +17,50 @@ vector<int> solution(string msg) {
     string str(ch);
     voca.push_back(str);
   }
+  string current, before;
+
+  for (int i = 0; i < msg.size(); i++) {
+    current = "";
+    before = "";
+    for (int j = i; j <= msg.size() - 1; j++) {
+      current = msg.substr(i, msg.size() - j);
+      // Search that is current string in vaca
+      if (isInVoca(voca, current)) {
+        answer.push_back(getVocaIndex(voca, current) + 1);
+        // Remove current input
+        i += msg.size() - j - 1;
+        break;
+      } else {
+        // Initialize next string that was not proceeded
+        before = current;
+        continue;
+      }
+    }
+    // Register next word into the voca
+    if (before != "") voca.push_back(before);
+  }
 
   return answer;
+}
+
+bool isInVoca(vector<string> voca, string s) {
+  bool result = false;
+  for (int i = 0; i < voca.size(); i++) {
+    if (voca[i] == s) {
+      result = true;
+      break;
+    }
+  }
+  return result;
+}
+
+int getVocaIndex(vector<string> voca, string s) {
+  int result = 0;
+  for (int i = 0; i < voca.size(); i++) {
+    if (voca[i] == s) {
+      result = i;
+      break;
+    }
+  }
+  return result;
 }
