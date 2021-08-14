5a7f9cecc9a92e6bbf2dae3362ba5ed5362b979f
Cha_Y_S <vaite714@gmail.com>
2021-08-15 00:15:58 +0900

    추가: 수식 최대화 스켈레톤 추가

---
+++ Programmers-Level2/수식 최대화/main.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("100-200*300-500+20") << endl;
+
+  cout << solution("50*6-3*2") << endl;
+
+  return 0;
+}
---
+++ Programmers-Level2/수식 최대화/solution.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(string expression);
