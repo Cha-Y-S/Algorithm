19c3060af86a74ab00e0dd255351a3cc38448a7d
firemanCha <vaite714@gmail.com>
2021-08-14 17:16:53 +0900

    추가: n진수 게임 스켈레톤 추가

---
+++ Programmers-Level2/n진수 게임/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  cout << solution(2, 4, 2, 1) << endl;
+
+  cout << solution(16, 16, 2, 1) << endl;
+
+  cout << solution(16, 16, 2, 2) << endl;
+
+  return 0;
+}
