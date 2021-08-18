ae48c0cf8135d2d5ce4d1360726eb2fe409d5f1f
Cha_Y_S <vaite714@gmail.com>
2021-08-19 02:33:19 +0900

    추가: 후보키 스켈레톤 추가

---
+++ Programmers-Level2/후보키/main.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+
+int main() {
+  vector<vector<string>> relation;
+
+  cout << solution(relation = {{"100", "ryan", "music", "2"}, {"200", "apeach", "math", "2"}, {"300", "tube", "computer", "3"}, {"400", "con", "computer", "4"}, {"500", "muzi", "music", "3"}, {"600", "apeach", "music", "2"}}) << endl;
+
+  return 0;
+}
