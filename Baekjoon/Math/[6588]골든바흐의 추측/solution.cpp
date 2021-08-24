81aa386629dcfb572f856d65a1293f956b470fee
Cha_Y_S <vaite714@gmail.com>
2021-08-25 03:14:28 +0900

    추가: 2021/08/25 ICPC Math 문제 추가

---
+++ Baekjoon/Math/[6588]골든바흐의 추측/solution.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define endl "\n";
+#define MAX 1000001
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+
+  // Initialize sieve of eratosthenes
+  vector<bool> prime_number(MAX, true);
+  prime_number[0] = prime_number[1] = false;
+
+  for (int i = 2; i <= sqrt(MAX) + 1; i++) {
+    if (prime_number[i]) {
+      for (int j = i + i; j < MAX; j += i)
+        prime_number[j] = false;
+    }
+  }
+
+  while (true) {
+    cin >> n;
+
+    if (!n) break;
+
+    pair<int, int> operand = {1, 0};
+
+    for (int j = 3; j <= n / 2; j += 2) {
+      // if left and right of n are prime number, store it into operand
+      if (prime_number[j] && prime_number[n - j]) {
+        operand = {j, n - j};
+        break;
+      }
+    }
+
+    if (operand.first == 1 && !operand.second) {
+      cout << "Goldbach's conjecture is wrong.\n";
+    } else {
+      cout << n << " = " << operand.first << " + " << operand.second << "\n";
+    }
+  }
+
+  return 0;
+}
