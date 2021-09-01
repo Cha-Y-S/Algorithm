a50dec9d206efd3a898fad3bcb532de280b56d7f
firemanCha <vaite714@gmail.com>
2021-09-01 17:09:18 +0900

    추가: 모음 사전 스켈레톤 추가

---
+++ Programmers-Level2/모음 사전/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  cout << solution("AAAAE") << endl;
+
+  cout << solution("AAAE") << endl;
+
+  cout << solution("I") << endl;
+
+  cout << solution("EIO") << endl;
+
+  return 0;
+}
