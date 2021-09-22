e133f1c26366fac84e3a8b11e41325732967a3e9
Cha_Y_S <vaite714@gmail.com>
2021-09-22 11:55:27 +0900

    추가: 없는 숫자 더하기 스켈레톤 추가

---
+++ Programmers-Level1/없는 숫자 더하기/main.cpp
@@ -0,0 +1,11 @@
+#include "solution.h"
+
+int main() {
+  vector<int> numbers;
+
+  cout << solution(numbers = {1, 2, 3, 4, 6, 7, 8, 0}) << endl;
+
+  cout << solution(numbers = {5, 8, 4, 0, 6, 7, 9}) << endl;
+
+  return 0;
+}
