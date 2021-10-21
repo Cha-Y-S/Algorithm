44ed970106b7553a0f4064295e4ca22c43b361e5
firemanCha <vaite714@gmail.com>
2021-10-21 18:15:04 +0900

    추가: 2021/10/21 ICPC DP 문제 추가

---
+++ Baekjoon/DynamicProgramming/[9625]BABBA/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+vector<pii> machine;
+int K;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> K;
+
+  machine = vector<pii>(K + 1, {1, 0});
+
+  for (int i = 1; i <= K; i++) {
+    machine[i].first = machine[i - 1].second;
+    machine[i].second = machine[i - 1].first + machine[i - 1].second;
+  }
+
+  cout << machine[k].first << " " << machine[k].second << endl;
+
+  return 0;
+}
