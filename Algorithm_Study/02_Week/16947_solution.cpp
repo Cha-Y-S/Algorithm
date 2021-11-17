4ef5ad14930b9c480748c0363a2eb0e8a15396ba
Cha_Y_S <vaite714@gmail.com>
2021-11-17 13:07:21 +0900

    추가: 2021/11/17 문제 추가

--- Algorithm_Study/02_Week/16947_solution.cpp
+++ Algorithm_Study/02_Week/16947_solution.cpp
@@ -1,73 +1,116 @@
-#include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <queue>
 #include <vector>
 
 using namespace std;
 
-vector<vector<int>> graph;
-vector<bool> visited;
-bool cycle;
+#define endl "\n"
 
-void dfs(int p, int v, int cnt);
+vector<vector<int>> station;
+vector<bool> check;
+vector<bool> visited;
+vector<int> res;
+bool cycle;
+int N;
+
+void dfs(int cur, int start, int cnt);
+int bfs(int v);
 
 int main() {
   ios::sync_with_stdio(false);
-  cin.tie(0);
+  cin.tie(nullptr);
 
-  int n;
+  cin >> N;
 
-  cin >> n;
+  station = vector<vector<int>>(N);
+  visited = vector<bool>(N, false);
+  check = vector<bool>(N, false);
 
-  graph = vector<vector<int>>(n);
-  visited = vector<bool>(n, false);
-
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < N; i++) {
     int v1, v2;
 
     cin >> v1 >> v2;
 
-    graph[v1 - 1].push_back(v2 - 1);
-    graph[v2 - 1].push_back(v1 - 1);
+    station[v1 - 1].push_back(v2 - 1);
+    station[v2 - 1].push_back(v1 - 1);
   }
 
-  for (int i = 0; i < graph.size(); i++) {
-    sort(graph[i].begin(), graph[i].end());
+  for (int i = 0; i < N; i++) {
+    visited = vector<bool>(N, false);
+    cycle = false;
+
+    int start = i;
+
+    dfs(i, start, 0);
+
+    if (cycle) check[i] = true;
+
+    visited.clear();
   }
 
-  for (int i = 0; i < graph.size(); i++) {
-    cout << i + 1 << ": ";
-    for (int j = 0; j < graph[i].size(); j++) {
-      cout << graph[i][j] + 1 << " ";
+  for (int i = 0; i < N; i++) {
+    if (check[i]) {
+      res.push_back(0);
+      continue;
     }
-    cout << endl;
+    res.push_back(bfs(i));
+
+    visited.clear();
   }
 
-  dfs(0, 0, 0);
+  for (int i = 0; i < res.size(); i++)
+    cout << res[i] << " ";
   cout << endl;
-
-  for (int i = 0; i < visited.size(); i++) {
-    cout << visited[i] << " ";
-  }
 
   return 0;
 }
 
-void dfs(int p, int c, int cnt) {
-  visited[c] = true;
-
-  cout << c + 1 << " ";
-
-  for (int v : graph[c]) {
-    if (!visited[v])
-      dfs(c, v, cnt + 1);
-    else if (p != v && cnt >= 3) {
-      cycle = true;
-      return;
-    }
+void dfs(int cur, int start, int cnt) {
+  if (cur == start && cnt >= 2) {
+    cycle = true;
+    return;
   }
 
-  if (!cycle) {
-    visited[c] = false;
+  visited[cur] = true;
+
+  for (int i = 0; i < station[cur].size(); i++) {
+    int next = station[cur][i];
+
+    if (!visited[next])
+      dfs(next, start, cnt + 1);
+    else {
+      if (next == start && cnt >= 2) dfs(next, start, cnt);
+    }
+
+    if (cycle == true) return;
+  }
+}
+
+int bfs(int v) {
+  visited = vector<bool>(N, false);
+  queue<pair<int, int>> q;
+
+  q.push({v, 0});
+  visited[v] = true;
+
+  while (!q.empty()) {
+    int cur = q.front().first;
+    int cnt = q.front().second;
+
+    q.pop();
+
+    if (check[cur]) {
+      return cnt;
+    }
+
+    for (int i = 0; i < station[cur].size(); i++) {
+      int next = station[cur][i];
+
+      if (!visited[next]) {
+        visited[next] = true;
+        q.push({next, cnt + 1});
+      }
+    }
   }
 }
