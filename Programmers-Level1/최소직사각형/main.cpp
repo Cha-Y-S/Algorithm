5bae3a38d724a675ed0f308790d0e0c8d4f8a559
firemanCha <vaite714@gmail.com>
2021-12-02 18:47:05 +0900

    추가: 최소직사각형 스켈레톤 추가

---
+++ Programmers-Level1/최소직사각형/main.cpp
@@ -0,0 +1,13 @@
+#include "solution.h"
+
+int main() {
+  vector<vector<int>> sizes;
+
+  cout << solution(sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}}) << endl;
+
+  cout << solution(sizes = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}) << endl;
+
+  cout << solution(sizes = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}}) << endl;
+
+  return 0;
+}
