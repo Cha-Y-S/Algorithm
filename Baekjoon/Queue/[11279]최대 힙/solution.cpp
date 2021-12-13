95100961a142f1524ef0f19bdc8845b8a065f301
firemanCha <vaite714@gmail.com>
2021-12-13 21:28:56 +0900

    추가: 2021/12/13 ICPC Queue 문제 추가

---
+++ Baekjoon/Queue/[11279]최대 힙/solution.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define endl "\n"
+
+priority_queue<int> pq;
+int N, num;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    cin >> num;
+
+    if (num == 0) {
+      if (pq.empty())
+        cout << 0 << endl;
+      else {
+        cout << pq.top() << endl;
+        pq.pop();
+      }
+    } else
+      pq.push(num);
+  }
+
+  return 0;
+}
