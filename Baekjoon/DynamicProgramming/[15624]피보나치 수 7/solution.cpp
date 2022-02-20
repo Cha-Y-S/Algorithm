0654423f62f5865e9af5ac606c20f28dba46ff31
firemanCha <vaite714@gmail.com>
2022-02-20 19:13:30 +0900

    추가: 2022/02/20 ICPC DP 문제 추가

---
+++ Baekjoon/DynamicProgramming/[15624]피보나치 수 7/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define endl "\n"
+#define MAX 1'000'001
+#define MOD 1'000'000'007
+
+vector<int> dp;
+int N;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N;
+
+  dp = vector<int>(MAX);
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (int i = 2; i <= N; i++)
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+
+  cout << dp[N] << endl;
+
+  return 0;
+}
