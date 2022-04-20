d68b56ca3aedfbf7150f58ce3d408bb882153c87
firemancha <vaite714@gmail.com>
2022-04-20 22:39:05 +0900

    추가: 2022/04/20 ICPC DP 문제 추가

---
+++ Baekjoon/DynamicProgramming/[11053]가장 긴 증가하는 부분 수열/solution.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const inputData = fs.readFileSync("example.dat").toString().split("\n");
+// const inputData = fs.readFileSync("/dev/stdin").toString().split("\n");
+const N = Number(inputData[0]);
+const seq = inputData[1].split(" ").map((item) => Number(item));
+const dp = new Array(N).fill(1);
+
+for (let i = 1; i < N; i++) {
+  for (let j = i - 1; j >= 0; j--) {
+    if (seq[i] > seq[j]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
+
+console.log(Math.max(...dp));
