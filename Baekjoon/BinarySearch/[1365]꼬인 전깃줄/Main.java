bf8a49fd287191143b818a168f5e0c70d50aff32
firemancha <vaite714@gmail.com>
2022-04-23 12:21:40 +0900

    추가: 2022/04/23 ICPC BS 문제 추가

---
+++ Baekjoon/BinarySearch/[1365]꼬인 전깃줄/Main.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static ArrayList<Integer> lis;
+  public static int N, num;
+
+  public static int lower_bound(int value){
+    int low = 0;
+    int high = lis.size();
+
+    while(low < high){
+      int mid = (low + high) / 2;
+
+      if(value < lis.get(mid)){
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return high;
+  }
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    N = Integer.parseInt(br.readLine());
+    lis = new ArrayList<>();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i = 0; i < N; i++){
+      num = Integer.parseInt(st.nextToken());
+
+      int idx = lower_bound(num);
+
+      if(idx == lis.size()){
+        lis.add(num);
+      } else{
+        lis.set(idx, num);
+      }
+    }
+
+    bw.write((N - lis.size()) + "\n");
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
